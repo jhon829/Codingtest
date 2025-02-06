f473444cd091e39456f759e12d359e5ce49e76d6
John829 <82113591+jhon829@users.noreply.github.com>
2025-02-06 18:57:05 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -0,0 +1,15 @@
+def solution(age):
+    answer = ''
+    answer = str(age)
+    answer = answer.replace('0', 'a')
+    answer = answer.replace('1', 'b')
+    answer = answer.replace('2', 'c')
+    answer = answer.replace('3', 'd')
+    answer = answer.replace('4', 'e')
+    answer = answer.replace('5', 'f')
+    answer = answer.replace('6', 'g')
+    answer = answer.replace('7', 'h')
+    answer = answer.replace('8', 'i')
+    answer = answer.replace('9', 'j')
+    
+    return answer
