bd489c828800c9ca9022778a48c689278bb17dbc
John829 <82113591+jhon829@users.noreply.github.com>
2025-04-01 18:26:45 +0900

    [level 0] Title: 수 조작하기 1, Time: 8.17 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,14 +1,11 @@
 def solution(n, control):
-    answer = 0
     for i in control:
         if i == "w":
-            n += 1
+            n = n +1
         elif i == "s":
-            n -= 1
+            n = n -1
         elif i == "d":
-            n += 10
+            n = n +10
         else:
-            n -= 10
-            
-            
+            n = n -10
     return n
