b9e0001f7d253761bf57a3a28cab088ac660ae29
John829 <82113591+jhon829@users.noreply.github.com>
2025-04-07 11:14:57 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 9.39 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
@@ -0,0 +1,8 @@
+def solution(n):
+    result= 0
+    answer = []
+    answer = list(str(n))
+    for i in answer:
+        result = result + int(i)
+                              
+    return result
